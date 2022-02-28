3a45041ec2abbcf577bab4dafe624f7d99555754
Hamish Gibbs <hamishgibbs@gmail.com>
2022-02-28 10:07:00 +0000

    locate templates correctly

--- schemagen/api.py
+++ schemagen/api.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="static/templates")
 
 @app.get("/")
 async def root():
